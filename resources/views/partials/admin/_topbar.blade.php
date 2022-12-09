0cb86ffb2d616d7f69cb187c32b3ca2c209093aa
ArcyAmby <arambayec@iskolarngbayan.pup.edu.ph>
2022-12-09 21:02:57 +0800

    login UI

--- resources/views/partials/admin/_topbar.blade.php
+++ resources/views/partials/admin/_topbar.blade.php
@@ -177,12 +177,12 @@
               <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                   data-bs-toggle="dropdown">
                   <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                  <span class="d-none d-md-block dropdown-toggle ps-2">{{auth()->user()->name}}</span>
+                  <span class="d-none d-md-block dropdown-toggle ps-2"></span>
               </a><!-- End Profile Iamge Icon -->
 
               <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                   <li class="dropdown-header">
-                      <h6>{{auth()->user()->name}}</h6>
+                      <h6>{{auth()->user()->admin->firstName}} {{auth()->user()->admin->lastName}}</h6>
                       <span>Canteen Staff</span>
                   </li>
                   <li>
@@ -190,7 +190,7 @@
                   </li>
 
                   <li>
-                      <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                      <a class="dropdown-item d-flex align-items-center" href="{{ url('account') }}">
                           <i class="bi bi-person"></i>
                           <span>My Profile</span>
                       </a>
@@ -220,17 +220,60 @@
                   </li>
 
                   <li>
-                      <a class="dropdown-item d-flex align-items-center" href="#">
+                      <button type="submit" class="dropdown-item d-flex align-items-center" >
                           <i class="bi bi-box-arrow-right"></i>
-                          <span>Sign Out</span>
-                      </a>
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                         </a>
+                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                      </button>
+                    </form>
                   </li>
 
               </ul><!-- End Profile Dropdown Items -->
           </li><!-- End Profile Nav -->
+          {{-- <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul> --}}
 
       </ul>
   </nav><!-- End Icons Navigation -->
+  
 
 </header>
 {{-- End Topbar --}}
