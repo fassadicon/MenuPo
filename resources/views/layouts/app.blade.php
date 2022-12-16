14d5ec7fa760cb936104d046a34f9385273a4d32
ArcyAmby <arambayec@iskolarngbayan.pup.edu.ph>
2022-12-16 21:32:11 +0800

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <link href="{{ asset('admin/assets/css/loginStyle.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     
 </head>
@@ -107,5 +108,7 @@
     </div>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
 </body>
 </html>
