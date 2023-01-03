92edc4cdb93872ee4dfd005e6556808ac413b7c1
fassadicon <fassadicon@iskolarngbayan.pup.edu.ph>
2023-01-03 22:52:21 +0800

    app public in storage path

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -80,8 +80,8 @@ class UsersImport implements ToModel
         // MERGED NAME AND QR
         // $image2 = public_path('storage/admin/qrs/' . $studentID . '.png');
         // $image1 = public_path('storage/admin/names/' . $studentID . '.png');
-        $image2 = storage_path('admin/qrs/' . $studentID . '.png');
-        $image1 = storage_path('admin/names/' . $studentID . '.png');
+        $image2 = storage_path('app/public/admin/qrs/' . $studentID . '.png');
+        $image1 = storage_path('app/public/admin/names/' . $studentID . '.png');
         $image1 = imagecreatefromstring(file_get_contents($image1));
         $image2 = imagecreatefromstring(file_get_contents($image2));
         imagecopymerge($image1, $image2, 5, 20, 0, 0, 300, 300, 100);
