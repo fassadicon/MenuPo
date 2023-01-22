a680dc5e207be0fef484c5c6a91bffb9f1f0aa96
Ervin Bautista <bautistaervin7@gmail.com>
2023-01-22 19:06:11 +0800

    footer changes

--- resources/views/components/user/layout2.blade.php
+++ resources/views/components/user/layout2.blade.php
@@ -510,10 +510,10 @@
       <!--Footer-->
       <footer class="footer flex flex-col items-center justify-between p-10 border-t-4 border-t-primary bg-white sm:flex-row">
     
-        <p class="text-sm text-gray-600">© 2022 School Name | <a href="/terms-and-conditions" class="text-primary">Terms of Use</a> | <a href="/privacy-statement" class="text-primary">Privacy Statement</a></p>
+        <p class="text-sm text-gray-600">© 2022 Nuestra Seniora De Aranzazu Parochial School | <a href="/terms-and-conditions" class="text-primary">Terms of Use</a> | <a href="/privacy-statement" class="text-primary">Privacy Statement</a></p>
     
         <div class="flex -mx-2">
-          <p class="text-sm text-gray-600">Contact Us at: 09613326890 (email: sample@gmail.com) or visit us at: School Address
+          <p class="text-sm text-gray-600">Contact Us at: 09613326890 (email: menupo2022@gmail.com) or visit us at: San Mateo, Philippines
         </div>
     </footer>
 
